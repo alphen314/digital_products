31735ebbd1c08642fadb48d6717453c865d8565a
mohammad <chessee.player314@gmial.com>
2025-03-04 18:37:50 +0330

    products model created

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 class Catagory(models.Model):
     parent = models.ForeignKey('self',verbose_name=_('parent'),blank=True,null=True,on_delete=models.CASCADE)
@@ -11,12 +11,34 @@ class Catagory(models.Model):
     update_time =models.DateTimeField(_('update_time'),auto_now=True)
     class Meta:
         db_table = 'catagoreis'
-        verbos_name  = _('catagory')
+        verbose_name  = _('catagory')
         verbose_name_plural = _('catagories')
-    
+
+
 class Product(models.Model):
-    pass
+    titile = models.CharField(_('title'),max_length=50)
+    description  = models.TextField(_('description'),blank=True)
+    avatar = models.ImageField(_('avatar'),blank=True,upload_to='products/')
+    is_enable = models.BooleanField(_('is_enable'),default=True)
+    catagries = models.ManyToManyField('catagory',verbose_name= _('catagories'),blank=True)
+    created_time = models.DateTimeField(_('created_time'),auto_now_add=True)
+    update_time =models.DateTimeField(_('update_time'),auto_now=True)
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
 
 class File(models.Model):
-    pass
+    product = models.ForeignKey('Product',verbose_name=_('product'),on_delete=models.CASCADE)
+    titile = models.CharField(_('title'),max_length=50)
+    file = models.FileField(_('file'),upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(_('is_enable'),default=True)
+    created_time = models.DateTimeField(_('created_time'),auto_now_add=True)
+    update_time =models.DateTimeField(_('update_time'),auto_now=True)
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
+
 
